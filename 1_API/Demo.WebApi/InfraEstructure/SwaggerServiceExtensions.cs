84e8ce3a99644d2a45d5605c0bf9f0e4e069a983
marcus.barbosa <marcus.barbosa@simplevisionit.com>
2021-02-15 09:00:23 -0300

    removendo trechos de código comentados

--- 1_API/Demo.WebApi/InfraEstructure/SwaggerServiceExtensions.cs
+++ 1_API/Demo.WebApi/InfraEstructure/SwaggerServiceExtensions.cs
@@ -16,41 +16,6 @@ namespace Demo.WebApi.InfraEstructure
                 {
                     c.SwaggerDoc("v1", new OpenApiInfo { Title = "DemoApi", Version = "v1" });
                 });
-            // services.AddSwaggerGen(c =>
-            // {
-            //     c.SwaggerDoc("v1.0", new OpenApiInfo { Title = "SoftanPlan API v1.0", Version = "v1.0" });
-            //         //    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            //         //     {
-            //         //         Name = "Authorization",
-            //         //         Type = SecuritySchemeType.ApiKey,
-            //         //         Scheme = "Bearer",
-            //         //         BearerFormat = "JWT",
-            //         //         In = ParameterLocation.Header,
-            //         //         Description = "JWT Authorization header using the Bearer scheme."
-            //         //     });
-                        
-            //         //     c.AddSecurityRequirement(new OpenApiSecurityRequirement
-            //         //     {
-            //         //         {
-            //         //               new OpenApiSecurityScheme
-            //         //                 {
-            //         //                     Reference = new OpenApiReference
-            //         //                     { 
-            //         //                         Type = ReferenceType.SecurityScheme, 
-            //         //                         Id = "Bearer" 
-            //         //                     }
-            //         //                 },
-            //         //                 new string[] {}
-            //         //         }
-            //         //     });
-            // });
-
-            // services.AddAuthorization(c =>
-            // {
-            //     c.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
-            //     .RequireAuthenticatedUser()
-            //     .Build();
-            // });
 
             return services;
         }
